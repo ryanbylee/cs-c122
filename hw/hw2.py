92961ffa8a8a9d96b0e2d73fa21fb7f06cc0a1ef
Ryan Lee <ryanbylee@ucla.edu>
2025-01-22 20:31:21 -0800

    hw2 initial commit

---
+++ hw/hw2.py
@@ -0,0 +1,11 @@
+import sys
+from typing import List, Dict, Iterable, Tuple, Set
+
+def trie_construction(patterns: List[str]) -> List[Tuple[int, int, str]]:
+    """
+    Construct a trie from a collection of patterns.
+    """
+    trie = {}
+
+
+
